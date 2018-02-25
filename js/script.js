803b8bf94b439bcf1f0a0edb07011fb98b8db98d
Ariarzer <ariarzer@gmail.com>
2018-02-25 23:55:47 +0300

    Add checkbox for tasks

--- js/script.js
+++ js/script.js
@@ -1,19 +1,47 @@
 var taskList = document.getElementById("taskList");
 
-function insertDivWithClass(insert, parent, myClass ){
-  var newElem = document.createElement('div');
-  newElem.innerHTML = insert;
-  parent.insertBefore(newElem, parent.firstChild);
-  newElem.classList.add(myClass);
-}
+var counter = 0;
 
 const resetValueById = (id) => document.getElementById(id).value = '';
 
+function create(tag, attributes) {
+  var element = document.createElement(tag);
+  if (typeof attributes == 'object') {
+    for (var i in attributes) {
+      element.setAttribute( i, attributes[i] );
+    }
+  }
+  for (var i = 2; i < arguments.length; i++) {
+    var val = arguments[i];
+    if (typeof val == 'string') {val = document.createTextNode(val)};
+    element.appendChild(val);
+  }
+  return element;
+}
+
+function Task(text) {
+  this.text = text;
+  this.isDone = false;
+}
+
+function createTaskBlock(newTask) {
+  return create('div', {class: 'todo-list_task'},
+                create('input', Object.assign({type: 'checkbox', id: counter.toString()}, newTask.isDone === true ? { checked: ''} : {})),
+                create('label', {'for': counter.toString().padStart(3, '0')}, newTask.text));
+}
+
 function addTask()
 {
-  var newTask = document.getElementById("newTask").value;
+  var textFieldValue = document.getElementById("newTask").value;
 
-  insertDivWithClass(newTask, taskList, 'todo-list_task');
+  if(textFieldValue){
+    var newTask = new Task(textFieldValue);
 
-  resetValueById("newTask");
+    taskList.insertBefore(createTaskBlock(newTask), taskList.firstChild);
+
+    resetValueById("newTask");
+
+    counter++;
+  }
+
 }
