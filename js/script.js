1f9a588d54b429ead19e108b5e06684f5d364286
Sofi Valitova <ariarzer@gmail.com>
2018-03-07 01:54:59 +0300

    Now you can mark tasks made (#10)
    * Add checkbox for tasks
    
    * Change conniction script
    
    * Refactoring script and fix id
    
    * Formed classes
    
    * It works now
    
    * Fix id of button (tack => task)
    
    * TaskList bind to create-task-button and unput field
    
    * Now TaskList connect to the button and input field in constructor
    
    * Сorrect list properties of attributes
    
    * Added storage tasks in array and revise the way of generate their id
    
    * Add fuction for generate task's id
    
    * Fixed assinment of id to tasks
    
    * Amended array of saved tasks
    
    * Fix clearing of input field
    
    * Added storage of checkbox values
    
    * Method AddNewTask put into prototype
    
    * Restructed classes and added style for checkbox
    
    * Fix #7

--- js/script.js
+++ js/script.js
@@ -1,19 +1,59 @@
-var taskList = document.getElementById("taskList");
+var taskList = new TaskList(document.getElementById('task-list'));
 
-function insertDivWithClass(insert, parent, myClass ){
-  var newElem = document.createElement('div');
-  newElem.innerHTML = insert;
-  parent.insertBefore(newElem, parent.firstChild);
-  newElem.classList.add(myClass);
+function Task(id, text, isDone) {
+  this.id = id;
+  this.text = text;
+  this.isDone = isDone;
+
+  var idForCheckbox = this.id + '-checkox';
+
+  this.checkbox = create('input', {type: 'checkbox', 'id': idForCheckbox, class: 'task_is-done'});
+  this.label = create('label', {'for': idForCheckbox, class: 'task_text'});
+
+  var self = this;
+  this.checkbox.addEventListener('change', function () {
+    self.isDone = self.checkbox.checked;
+  });
+
+  this.block = create('div', {class: 'task', 'id': this.id}, this.checkbox, this.label, this.text);
 }
 
-const resetValueById = (id) => document.getElementById(id).value = '';
+var getTaskId = (i) => ('my-task-' + (i).toString().padStart(3, '0'));
 
-function addTask()
-{
-  var newTask = document.getElementById("newTask").value;
+function TaskList(block) {
+  this.block = block;
+  this.button = document.getElementById('create-task-button');
+  this.textArea = document.getElementById('new-task');
+  this.tasks = [];
 
-  insertDivWithClass(newTask, taskList, 'todo-list_task');
+  var self = this;
+  this.button.addEventListener('click', function () {
+    var taskText = self.textArea.value;
+    if (taskText){
+      self.addNewTask(taskText);
+      self.textArea.value = '';
+    }
+  });
+}
 
-  resetValueById("newTask");
+TaskList.prototype.addNewTask = function (taskText){
+  idNewTask = getTaskId(this.tasks.length);
+  newTask = new Task(idNewTask, taskText, false);
+  this.block.insertBefore(newTask.block, this.block.firstChild);
+  this.tasks.push(newTask);
+}
+
+function create(tag, attributes) {
+  var element = document.createElement(tag);
+  if (typeof attributes == 'object') {
+    Object.keys(attributes).forEach((key) => {
+      element.setAttribute( key, attributes[key] );
+    });
+  }
+  for (var i = 2; i < arguments.length; i++) {
+    var val = arguments[i];
+    if (typeof val == 'string') {val = document.createTextNode(val)};
+    element.appendChild(val);
+  }
+  return element;
 }
